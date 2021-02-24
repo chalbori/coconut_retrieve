d9fa751858eee6305dc72af3677733de4a830482
Sangwon Lee <sw.lee@yonsei.ac.kr>
2021-02-24 22:00:50 +0900

    test test test

--- test_connect.py
+++ test_connect.py
@@ -1,4 +1,6 @@
 from database.coconut import COCONUT
+from data import source_np
+from pprint import pprint
 
 repo = COCONUT()
 # print(repo.count())
@@ -6,7 +8,13 @@ repo = COCONUT()
 i = 0
 for np in repo.get_unique_stream():
     print(np)
+
     i = i + 1
 
     if i > 5:
         break
+
+# print(repo.get_count_source(source="gnps"))
+# print(repo.get_unique_source_statistics())
+
+print(repo.get_source_organism_set())
