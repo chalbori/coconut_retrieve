b8561012deea099a5fd748114815bda50054738b
Sangwon Lee <sw.lee@yonsei.ac.kr>
2021-03-14 03:17:07 +0900

    Add mol writer

--- valid_unique_np.py
+++ valid_unique_np.py
@@ -3,6 +3,8 @@ from data.unique_np import Unique_NP
 from pprint import pprint
 from rdkit import Chem
 from rdkit.Chem import inchi
+from rdkit.Chem.rdchem import Mol
+from rdkit.Chem.rdmolfiles import SDWriter
 
 repo = COCONUT()
 # print(repo.count())
@@ -15,10 +17,13 @@ converted_list_file = open(
 converted_not_match_file = open(
     "out/inchi_valid_check/converted_not_match.txt", mode="w", encoding="utf-8"
 )
+w = SDWriter("out/inchi_valid_check/converted.sdf")
 np: Unique_NP
 for np in repo.get_unique_stream():
+    mol = Mol()
     try:
         mol = Chem.MolFromInchi(inchi=np.inchi, treatWarningAsError=True)
+        mol.SetProp("coconut_id",np.coconut_id)
     except:
         not_converted += 1
 
@@ -27,15 +32,21 @@ for np in repo.get_unique_stream():
 
         if np.inchikey == mol_inchikey:
             converted_list_file.write(np.inchi + "\n")
+            w.write(mol)
             converted += 1
         else:
             converted_not_match_file.write(np.inchi + "\n")
             not_converted += 1
     i += 1
+    del np
 
-    if i % 10000 == 0:
+    if i % 1000 == 0:
         print("{}th checked".format(i))
 
+    # if i > 5:
+    #     break
 print("converted:", converted, "not converted:", not_converted)
+w.close()
 converted_list_file.close()
 converted_not_match_file.close()
+del repo
